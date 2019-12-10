341bd892909c271601986a450287c29cc923ed03
GregArlg <55094487+GregArlg@users.noreply.github.com>
2019-12-10 16:20:46 +0100

    Ajout Fragment Google Maps avec ma clé API valide générée sur google Fragment fonctionnel affichage carte et marqueurs

--- app/src/main/java/com/ismin/opendataapp/MainActivity.kt
+++ app/src/main/java/com/ismin/opendataapp/MainActivity.kt
@@ -3,50 +3,53 @@ package com.ismin.opendataapp
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.stationsservicemapfr.InfoFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var station: Station
+    private lateinit var stationTest: Station
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Fragment affiché à l'ouverture
+        putFragment(StationListFragment())
+
+
+
         // Boutons
 
-        //Appelle le fragment Info au clic sur le bouton info
-        infoBtn.setOnClickListener { putInfoFragment()  }
+        //Appelle le fragment List au clic sur le bouton map
+        listeBtn.setOnClickListener { putFragment(StationListFragment())  }
 
         //Appelle le fragment Map au clic sur le bouton map
-        carteBtn.setOnClickListener { putMapFragment()  }
+        carteBtn.setOnClickListener { putFragment(MapFragment())  }
 
-        station
+        //Appelle le fragment Info au clic sur le bouton info
+        infoBtn.setOnClickListener { putFragment(InfoFragment())  }
+
+
+
+        //Station test à Marseille
+        stationTest = Station(43.3f, 5.4f, 11111, "TestPump", "La pompe test")
+
     }
 
 
     /**
-     * Fct d'appel du fragment InfoFragment
+     * Fct d'appel du fragment en paramètre
      */
-    private fun putInfoFragment(){
-        val fragment = InfoFragment()
+    private fun putFragment(fragment: Fragment){
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.a_main_rootview, fragment)
         fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
         fragmentTransaction.commit()
     }
 
-    /**
-     * Fct d'appel du fragment MapFragment
-     */
-    private fun putMapFragment(){
-        val fragment = MapFragment()
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.a_main_rootview, fragment)
-        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-        fragmentTransaction.commit()
-    }
+
 
 }
---
+++ app/src/main/java/com/ismin/opendataapp/StationListFragment.kt
@@ -0,0 +1,47 @@
+package com.ismin.opendataapp
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class StationListFragment : Fragment() {
+    private var listener: OnStationListListener? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_station_list, container, false)
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    /*fun onButtonPressed(uri: Uri) {
+        listener?.onInfoClic(uri)
+    }*/
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        /*   if (context is OnInfoClicListener) {
+               listener = context
+           } else {
+               throw RuntimeException(context.toString() + " must implement OnInfoClicListener")
+           }
+           */
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+
+    interface OnStationListListener {
+        // TODO: Update argument type and name
+        // fun onInfoClic(uri: Uri)
+    }
+}
